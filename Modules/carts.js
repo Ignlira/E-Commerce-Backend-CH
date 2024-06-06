c17633e012c5e2166a4faf5d30f63e878e3b981a
Ignacio Lira <ignlira@gmail.com>
2024-06-06 19:12:32 -0400

    error de nombre de carpeta

--- Modules/carts.js
+++ Modules/carts.js
@@ -2,10 +2,7 @@ const fs = require('fs-extra');
 const path = require('path');
 const { createCart } = require('./models/cart');
 
-
-
 const cartsFilePath = path.join(__dirname, 'data', 'carts.json');
-
 
 const getCarts = async () => {
   const cartsData = await fs.readJson(cartsFilePath);
