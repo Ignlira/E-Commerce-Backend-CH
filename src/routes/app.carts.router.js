2c67b237597c8f40e36046d5fab9319d20650fab
Ignacio Lira <ignlira@gmail.com>
2024-07-12 15:29:32 -0400

    ecommerce_02

--- src/routes/app.carts.router.js
+++ src/routes/app.carts.router.js
@@ -1,15 +1,15 @@
 import { Router } from "express";
-import CoursesManager from "../managers/CoursesManager.js";
+import CartsManager from "../managers/CartsManager.js";
 
 import { ERROR_SERVER } from "../constants/messages.constant.js";
 
 const router = Router();
-const coursesManager = new CoursesManager();
+const cartsManager = new cartsManager();
 
 router.get("/", async (req, res) => {
     try {
-        const courses = await coursesManager.getAll(req.query);
-        res.status(200).render("courses", { title: "Cursos", courses: courses.docs });
+        const carts = await cartsManager.getAll(req.query);
+        res.status(200).render("carts", { title: "Cursos", carts: carts.docs });
     } catch (error) {
         res.status(500).send(`<h1>Error 500</h1><h3>${ERROR_SERVER}</h3>`);
     }
